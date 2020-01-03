de0f4e50e7f39999e37066f068becbaae098d712
ueslei <uesleijf@gmail.com>
2020-01-03 13:25:55 -0300

    - refactory Model Cidades

--- basico/models/Cidades.php
+++ basico/models/Cidades.php
@@ -2,10 +2,45 @@
 
 namespace app\models;
 
+use Yii;
 
-use yii\db\ActiveRecord;
-
-class Cidades extends ActiveRecord
+/**
+ * This is the model class for table "Cidades".
+ *
+ * @property int $id
+ * @property string|null $nome
+ */
+class Cidades extends \yii\db\ActiveRecord
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'Cidades';
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'required'],
+            [['id'], 'integer'],
+            [['nome'], 'string', 'max' => 255],
+            [['id'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+        ];
+    }
 }

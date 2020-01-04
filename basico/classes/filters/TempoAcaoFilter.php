3bee7b702251d5ffc3259363a6d17afe73807c5b
ueslei <uesleijf@gmail.com>
2020-01-04 15:39:54 -0300

    - Create classes/filters/TempoAcaoFilter.php

---
+++ basico/classes/filters/TempoAcaoFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\classes\filters;
+
+
+use yii\base\ActionFilter;
+
+class TempoAcaoFilter extends ActionFilter
+{
+
+    public function beforeAction($action)
+    {
+
+    }
+
+    public function afterAction($action, $result)
+    {
+        
+    }
+
+}

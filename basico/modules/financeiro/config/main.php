d8e94171cb105dfd0fc2e37f5cbcd15b3cecda7c
ueslei <uesleijf@gmail.com>
2020-01-04 16:07:36 -0300

    - Configure modules/financeiro/config/main.php

---
+++ basico/modules/financeiro/config/main.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'components' => [
+        // Components somente desse módulo!
+    ],
+    'params' => [
+        'limiteCredito' => 12000,
+        'centroDeCusto' => 'Recursos Humanos',
+        'aceitarValorNegativo' => true
+    ]
+];

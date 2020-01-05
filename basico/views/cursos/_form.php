6762260c7806bdfdab83d3891a30cec7f00daf18
ueslei <uesleijf@gmail.com>
2020-01-05 14:56:04 -0300

    - Create basico/views/cursos/_form.php

---
+++ basico/views/cursos/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Cursos */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="cursos-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'nome')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'area')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'turno')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>

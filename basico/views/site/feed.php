dc34ff99605569f1c52d15fe6d03f45a97b9cd24
ueslei <uesleijf@gmail.com>
2020-01-06 10:59:49 -0300

    - Create View feed.php

---
+++ basico/views/site/feed.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+
+use app\classes\widgets\HelloWidget;
+use app\classes\widgets\HelloWordBeginEnd;
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+
+    <div class="body-content">
+        <h1>Feed de Notícias via REST API</h1>
+    </div>
+</div>

0db89c41ea6ca463fc0dca0977275142bfb0c035
ueslei <uesleijf@gmail.com>
2020-01-07 10:53:08 -0300

    - Create Migrations

---
+++ basico/migrations/m200107_134921_create_categoria.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200107_134921_create_categoria
+ */
+class m200107_134921_create_categoria extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200107_134921_create_categoria cannot be reverted.\n";
+
+        return false;
+    }
+
+}

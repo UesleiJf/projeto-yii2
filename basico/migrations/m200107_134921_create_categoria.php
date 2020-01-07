d6d397d9c1f3c09aa3dd085d92183a757b04abb2
ueslei <uesleijf@gmail.com>
2020-01-07 10:54:40 -0300

    - Create function to create Table

--- basico/migrations/m200107_134921_create_categoria.php
+++ basico/migrations/m200107_134921_create_categoria.php
@@ -12,7 +12,17 @@ class m200107_134921_create_categoria extends Migration
      */
     public function safeUp()
     {
+        $this-$this->createTable('categorias', [
+            'id' => $this->primaryKey(),
+            'nome' => $this->string(60)->notNull(),
+            'data_cadastro' => $this->dateTime()->notNull(),
+        ]);
+    }
 
+
+    public function down()
+    {
+        $this->dropTable('categorias');
     }
 
     /**

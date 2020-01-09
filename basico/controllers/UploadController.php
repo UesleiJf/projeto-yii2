7a255486e3d86089cf3d84ebb19bc77c6ad50d8a
ueslei <uesleijf@gmail.com>
2020-01-09 10:28:08 -0300

    - Controller uploadcontroller

--- basico/controllers/UploadController.php
+++ basico/controllers/UploadController.php
@@ -10,7 +10,9 @@ namespace app\controllers;
 
 
 use app\models\Clientes;
+use Yii;
 use yii\base\Controller;
+use yii\web\UploadedFile;
 
 class UploadController extends Controller
 {
@@ -21,7 +23,13 @@ class UploadController extends Controller
         $model = new Clientes;
 
         if ($model->load($post) && $model->validate()){
+            $model->fotoCliente = UploadedFile::getInstance($model, 'fotoCliente');
 
+            $model->foto = $model->fotoCliente->name;
+            $model->save();
+
+            $uploadPath = Yii::getAlias('@webroot/files');
+            $model->fotoCliente->saveAs($uploadPath . '/' .  $model->fotoCliente->name);
         }
 
         return $this->render('index', [

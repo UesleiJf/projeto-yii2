82a4ecdec079a68327beccf6e5d8af4d97b59f95
ueslei <uesleijf@gmail.com>
2020-01-09 11:03:46 -0300

    - Create CadastroForm to validate fields

---
+++ basico/models/CadastroForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+
+class CadastroForm extends Model
+{
+    public $nome;
+    public $email;
+    public $idade;
+    public $site;
+    public $dataNascimento;
+    public $dataInicial;
+    public $dataFinal;
+
+    public function rules()
+    {
+        return [
+            ['nome', 'required']
+        ]
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'nome' => 'Nome',
+            'email' => 'Email',
+            'idade' => 'Idade',
+            'site' => 'Site/Home Page',
+            'dataNascimento' => 'Data Nascimento',
+            'dataInicial' => 'Data Inicial',
+            'dataFinal' => 'Data Final',
+        ];
+    }
+}

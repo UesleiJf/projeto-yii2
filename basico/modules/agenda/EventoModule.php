da375104c397c152e1aa7b2c9711053412144326
ueslei <uesleijf@gmail.com>
2020-01-11 13:26:47 -0300

    - Create EventoModule

---
+++ basico/modules/agenda/EventoModule.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\agenda;
+
+/**
+ * evento module definition class
+ */
+class EventoModule extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\agenda\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}

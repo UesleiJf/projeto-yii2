b211c56e36c25c42984b9f06c9abbea86f0f7d8d
ueslei <uesleijf@gmail.com>
2020-01-13 16:29:13 -0300

    - Create model Sizes

---
+++ basico/models/Sizes.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%sizes}}".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $abbreviation
+ * @property int $status
+ * @property string|null $created_at
+ * @property string|null $updated_at
+ */
+class Sizes extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%sizes}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'abbreviation'], 'required'],
+            [['status'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['name'], 'string', 'max' => 60],
+            [['abbreviation'], 'string', 'max' => 10],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Nome',
+            'abbreviation' => 'Abreviação',
+            'status' => 'Status',
+            'created_at' => 'Criado em',
+            'updated_at' => 'Atualizado em',
+        ];
+    }
+}

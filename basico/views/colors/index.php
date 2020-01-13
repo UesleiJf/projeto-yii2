38ceee8e4be25aeaa380c4be321f1685a5ee9cde
ueslei <uesleijf@gmail.com>
2020-01-13 16:28:41 -0300

    - Refactory index view Colors to pagination and size columns

--- basico/views/colors/index.php
+++ basico/views/colors/index.php
@@ -1,12 +1,34 @@
 <?php
+use yii\grid\GridView;
+
 $this->title = 'Colors';
 ?>
 
 <div class="site-index">
     <div class="body-content">
-        <h1>Agenda via REST API</h1>
+        <h1>Cores!!!</h1>
 
-        
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                [
+                    'attribute' => 'id',
+                    'headerOptions' => [
+                        'style' => 'text-align: right; width: 70px;'
+                    ],
+                    'contentOptions' => [
+                        'style' => 'text-align: right;'
+                    ]
+                ],
+                'created_at',
+                [
+                    'attribute' => 'name',
+                    'content' => function(\app\models\Sizes $model, $key, $index, $column){
+                        return $model->name . "({$model->abbreviation})";
+                    }
+                ]
+            ]
+        ]); ?>
 
     </div>
 </div>

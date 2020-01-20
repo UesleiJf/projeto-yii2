ca84d489e5ef6ab12c71ef8bf2b0eb8ac41a414f
ueslei <uesleijf@gmail.com>
2020-01-20 17:10:12 -0300

    - Create Index View Funcionarios

---
+++ basico/views/funcionarios/index.php
@@ -0,0 +1,12 @@
+<?php
+use yii\helpers\StringHelper;
+
+$this->title = 'Funcionários';
+?>
+
+<div class="site-index">
+    <div class="body-content">
+        <h1>Funcionários</h1>
+
+    </div>
+</div>

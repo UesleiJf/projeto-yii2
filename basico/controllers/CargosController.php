0669ea3bcb745ef998aaaceec75c8b50c35ea26d
ueslei <uesleijf@gmail.com>
2020-01-20 17:25:20 -0300

    - FK controllers/CargosController.php

--- basico/controllers/CargosController.php
+++ basico/controllers/CargosController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 
+use app\models\Cargo;
 use yii\base\Controller;
 
 class CargosController extends Controller
@@ -10,6 +11,23 @@ class CargosController extends Controller
 
     public function actionIndex()
     {
+        /** @var $cargo[] $cargos */
+        $cargos = Cargo::find()->all();
+
+        foreach ($cargos as $cargo){
+            echo"<h2>";
+                echo $cargo->nome;
+
+                echo "<ul>";
+                    foreach ($cargo->funcionarios as $func){
+                        echo "<li>{$func->nome}</li>";
+                    }
+                echo "</ul>";
+            echo "</h2>";
+        }
+
+        exit;
+
         return $this->render('index');
     }
 

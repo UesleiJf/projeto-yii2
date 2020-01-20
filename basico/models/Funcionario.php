53242e56e012bd64b5eb479c3ceae06f5a3815ad
ueslei <uesleijf@gmail.com>
2020-01-20 17:26:42 -0300

    - Create relation Funcionario/Cargo

--- basico/models/Funcionario.php
+++ basico/models/Funcionario.php
@@ -1,15 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hunk
- * Date: 20/01/20
- * Time: 17:53
- */
 
 namespace app\models;
 
 
-class Funcionario
-{
+use yii\db\ActiveRecord;
 
+/**
+ * Class Funcionario
+ * @package app\models
+ *
+ * @property int $id
+ * @property int $cargo_id
+ * @property string $nome
+ * @property Cargo $cargo
+ */
+class Funcionario extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return 'Funcionarios';
+    }
+
+    public function rules()
+    {
+        return [
+            [['nome', 'cargo_id'], 'required'],
+            [['cargo_id'], 'integer'],
+            ['nome', 'string', 'max' => 60]
+        ];
+    }
+
+    public function getCargo()
+    {
+        return $this->hasOne(Cargo::class, ['id' => 'cargo_id']);
+    }
 }

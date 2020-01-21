1df3b01ceb4472d6746cf5cb4fce185ac8bbdb57
ueslei <uesleijf@gmail.com>
2020-01-21 13:05:54 -0300

    - Create Model PessoaJuridica

--- basico/models/PessoaJuridica.php
+++ basico/models/PessoaJuridica.php
@@ -2,8 +2,37 @@
 
 namespace app\models;
 
+use yii\db\ActiveRecord;
 
-class PessoaJuridica
+/**
+ * Class PessoaJuridica
+ * @package app\models
+ *
+ * @property int $pessoa_id
+ * @property string $cnpj
+ * @property string $ie
+ * @property Pessoas $pessoa
+ */
+class PessoaJuridica extends ActiveRecord
 {
 
+    public static function tableName()
+    {
+        return 'Pessoa_Juridica';
+    }
+
+    public function rules()
+    {
+        return [
+            [['pessoa_id', 'cnpj', 'ie'], 'required'],
+            ['pessoa_id', 'integer'],
+            ['cnpj', 'string', 45],
+            ['ie', 'string', 45],
+        ];
+    }
+
+    public function getPessoa()
+    {
+        return $this->hasOne(Pessoas::class, ['id' => 'pessoa_id']);
+    }
 }

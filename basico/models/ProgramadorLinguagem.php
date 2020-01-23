f5005b562e6a92a6bec1cc14385d587c39a17dcf
ueslei <uesleijf@gmail.com>
2020-01-23 16:30:14 -0300

    - Create rules, tableName and gets Programador and Linguagens

--- basico/models/ProgramadorLinguagem.php
+++ basico/models/ProgramadorLinguagem.php
@@ -3,8 +3,32 @@
 namespace app\models;
 
 
-class ProgramadorLinguagem
+use yii\db\ActiveRecord;
+
+class ProgramadorLinguagem extends ActiveRecord
 
 {
+    public static function tableName()
+    {
+        return 'programadores_linguagens';
+    }
 
+    public function rules(){
+
+        return [
+            [['programador_id', 'linguagem_id'], 'required'],
+            [['programador_id', 'linguagem_id'], 'integer'],
+            [['programador_id', 'linguagem_id'], 'unique'],
+        ];
+    }
+
+    public function getProgramador()
+    {
+
+    }
+
+    public function getLinguagem()
+    {
+        
+    }
 }

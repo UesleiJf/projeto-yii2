7522fdbaf3d341b046df575eb87bf08e3e893b1f
ueslei <uesleijf@gmail.com>
2020-01-27 15:13:12 -0300

    - Create Relations

--- basico/models/Linguagem.php
+++ basico/models/Linguagem.php
@@ -1,15 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hunk
- * Date: 27/01/20
- * Time: 16:02
- */
 
 namespace app\models;
 
 
-class Linguagem
+use yii\db\ActiveRecord;
+
+/**
+ * Class Linguagem
+ * @package app\models
+ *
+ * @property int $id
+ * @property string nome
+ * @property ProgramadorLinguagem[] $programadorLinguagens
+ * @property Programador[] $programadores
+ */
+
+class Linguagem extends ActiveRecord
 {
+
+    public static function()
+    {
+        return 'linguagens';
+    }
+
+    public function rules()
+    {
+        return [
+            [['nome'], 'required'],
+            [['nome'], 'string', 'max' => 60]
+        ];
+    }
+
+    public function getProgramadorLinguagens()
+    {
+        return $this->hasMany(ProgramadorLinguagem::class, ['linguagem_id' => 'id']);
+    }
+
+    public function getProgramadores()
+    {
+        return $this->hasMany(Programador::class, ['id' => 'programador_id'])
+            ->viaTable(Programador::tableName(), ['programador_id' => 'id']);
+    }
 
 }
